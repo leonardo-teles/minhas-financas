390a4ef34cb47b0bc64cc2f3939a80adf3ea9c98
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-04-01 17:11:00 -0300

    Back-end Criado

---
+++ backend/src/test/java/com/financas/FinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financas;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
