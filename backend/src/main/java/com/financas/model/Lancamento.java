f2c2cafabc25d0b4998582a6b56ef3cac75fb3d7
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-04-01 22:52:49 -0300

    Mapeando as Entidades Usuário e Lançamento

---
+++ backend/src/main/java/com/financas/model/Lancamento.java
@@ -0,0 +1,28 @@
+package com.financas.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "lancamento", schema = "financas")
+public class Lancamento {
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "nome")
+	private String nome;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "senha")
+	private String senha;
+
+}
