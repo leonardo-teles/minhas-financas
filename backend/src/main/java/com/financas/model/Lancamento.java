de7952d2545f7368b81e7d39863beb0acec01d4b
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-04-01 23:27:41 -0300

    Refatorando Entidades e Adicionando Anotações do Lombok

--- backend/src/main/java/com/financas/model/Lancamento.java
+++ backend/src/main/java/com/financas/model/Lancamento.java
@@ -20,7 +20,10 @@ import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import com.financas.enums.StatusLancamento;
 import com.financas.enums.TipoLancamento;
 
+import lombok.Data;
+
 @Entity
+@Data
 @Table(name = "lancamento", schema = "financas")
 public class Lancamento {
 
@@ -58,107 +61,4 @@ public class Lancamento {
 	@JoinColumn(name = "id_usuario")
 	private Usuario usuario;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getDescricao() {
-		return descricao;
-	}
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-
-	public Integer getMes() {
-		return mes;
-	}
-
-	public void setMes(Integer mes) {
-		this.mes = mes;
-	}
-
-	public Integer getAno() {
-		return ano;
-	}
-
-	public void setAno(Integer ano) {
-		this.ano = ano;
-	}
-
-	public BigDecimal getValor() {
-		return valor;
-	}
-
-	public void setValor(BigDecimal valor) {
-		this.valor = valor;
-	}
-
-	public LocalDate getDataCadastro() {
-		return dataCadastro;
-	}
-
-	public void setDataCadastro(LocalDate dataCadastro) {
-		this.dataCadastro = dataCadastro;
-	}
-
-	public TipoLancamento getTipo() {
-		return tipo;
-	}
-
-	public void setTipo(TipoLancamento tipo) {
-		this.tipo = tipo;
-	}
-
-	public StatusLancamento getStatus() {
-		return status;
-	}
-
-	public void setStatus(StatusLancamento status) {
-		this.status = status;
-	}
-
-	public Usuario getUsuario() {
-		return usuario;
-	}
-
-	public void setUsuario(Usuario usuario) {
-		this.usuario = usuario;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Lancamento other = (Lancamento) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-
-	@Override
-	public String toString() {
-		return "Lancamento [id=" + id + ", descricao=" + descricao + ", mes=" + mes + ", ano=" + ano + ", valor="
-				+ valor + ", dataCadastro=" + dataCadastro + ", tipo=" + tipo + ", status=" + status + ", usuario="
-				+ usuario + "]";
-	}
 }
