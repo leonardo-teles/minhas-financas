b61f97bdddcf90f117e384d12ab90d07524c1300
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-04-04 01:39:16 -0300

    Implementação do POST de Usuários

--- backend/src/main/java/com/financas/resource/UsuarioResource.java
+++ backend/src/main/java/com/financas/resource/UsuarioResource.java
@@ -1,13 +1,38 @@
 package com.financas.resource;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.financas.dto.UsuarioDTO;
+import com.financas.exception.RegraNegocioException;
+import com.financas.model.Usuario;
+import com.financas.service.UsuarioService;
+
 @RestController
+@RequestMapping("/api/usuarios")
 public class UsuarioResource {
 
-	@GetMapping("/")
-	public String ola( ) {
-		return "olá";
+	private UsuarioService usuarioService;
+	
+	public UsuarioResource(UsuarioService usuarioService) {
+		this.usuarioService = usuarioService;
+	}
+	
+	@PostMapping
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public ResponseEntity salvar(@RequestBody UsuarioDTO usuarioDTO) {
+		Usuario usuario = Usuario.builder().nome(usuarioDTO.getNome()).email(usuarioDTO.getEmail()).senha(usuarioDTO.getSenha()).build();
+		
+		try {
+			Usuario usuarioSalvo = usuarioService.salvarUsuario(usuario);
+			
+			return new ResponseEntity(usuarioSalvo, HttpStatus.CREATED);
+		} catch (RegraNegocioException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
 	}
 }
