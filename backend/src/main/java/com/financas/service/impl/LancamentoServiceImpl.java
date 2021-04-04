29de6dd2ffe21aa379f4bc73c4577d05bc041f9f
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-04-04 20:20:56 -0300

    Implementando Método de Buscar/Filtrar Lançamento

--- backend/src/main/java/com/financas/service/impl/LancamentoServiceImpl.java
+++ backend/src/main/java/com/financas/service/impl/LancamentoServiceImpl.java
@@ -1,7 +1,11 @@
 package com.financas.service.impl;
 
 import java.util.List;
+import java.util.Objects;
 
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.ExampleMatcher.StringMatcher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,27 +30,35 @@ public class LancamentoServiceImpl implements LancamentoService {
 	}
 
 	@Override
+	@Transactional
 	public Lancamento atualizar(Lancamento lancamento) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void deletar(Lancamento lancamento) {
-		// TODO Auto-generated method stub
+		Objects.requireNonNull(lancamento.getId());
 		
+		return lancamentoRepository.save(lancamento);
 	}
 
 	@Override
+	@Transactional
+	public void deletar(Lancamento lancamento) {
+		Objects.requireNonNull(lancamento.getId());
+		
+		lancamentoRepository.delete(lancamento);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public List<Lancamento> buscar(Lancamento lancamentoFiltro) {
-		// TODO Auto-generated method stub
-		return null;
+		Example example = Example.of(lancamentoFiltro, ExampleMatcher.matching().withIgnoreCase().withStringMatcher(StringMatcher.CONTAINING));
+		
+		return lancamentoRepository.findAll(example);
 	}
 
 	@Override
 	public void atualizarStatus(Lancamento lancamento, StatusLancamento status) {
-		// TODO Auto-generated method stub
+		lancamento.setStatus(status);
 		
+		atualizar(lancamento);
 	}
 
 }
