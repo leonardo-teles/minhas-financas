51192a7f30a82a7f23febf5cfa10007678bae0b1
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-04-07 17:35:03 -0300

    Componentizando o Formulário de Login

--- frontend/financas/src/views/login.js
+++ frontend/financas/src/views/login.js
@@ -1,5 +1,7 @@
 import React from 'react';
+
 import Card from '../components/card';
+import FormGroup from '../components/form-group';
 
 class Login extends React.Component {
     render() {
@@ -9,7 +11,33 @@ class Login extends React.Component {
                     <div className="col-md-6" style={ {position: 'relative', left: '300px'} }>
                         <div className="bs-docs-section">
                             <Card title="Login">
-                                Meu cartão
+                                <div className="row">
+                                    <div className="col-lg-12">
+                                        <div className="bs-component">
+                                            <fieldset>
+
+                                                <FormGroup label="e-Mail: *" htmlFor="email">   
+                                                    <input type="email" 
+                                                           className="form-control" 
+                                                           id="email" 
+                                                           aria-describedby="email" 
+                                                           placeholder="Digite o e-Mail"
+                                                    />
+                                                </FormGroup>
+
+                                                <FormGroup label="Senha: *" htmlFor="senha">   
+                                                    <input type="password" 
+                                                           className="form-control" 
+                                                           id="senha" 
+                                                           aria-describedby="senha" 
+                                                           placeholder="Digite a senha"
+                                                    />
+                                                </FormGroup>
+
+                                            </fieldset>
+                                        </div>
+                                    </div>
+                                </div>
                             </Card>
                         </div>
                     </div>
