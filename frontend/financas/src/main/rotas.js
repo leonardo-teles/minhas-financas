bc28975a0cae7a96e63aaec2a77cc99599057128
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-04-07 23:05:34 -0300

    Criando a Página Home

--- frontend/financas/src/main/rotas.js
+++ frontend/financas/src/main/rotas.js
@@ -1,13 +1,15 @@
 import React from 'react';
-
 import { Route, Switch, HashRouter } from 'react-router-dom';
-import CadastroUsuario from '../views/cadastroUsuario';
+
+import Home from '../views/home';
 import Login from '../views/login';
+import CadastroUsuario from '../views/cadastroUsuario';
 
 function Rotas() {
     return(
         <HashRouter>
             <Switch>
+                <Route path="/home" component={Home}/>
                 <Route path="/login" component={Login}/>
                 <Route path="/cadastro-usuario" component={CadastroUsuario}/>
             </Switch>
