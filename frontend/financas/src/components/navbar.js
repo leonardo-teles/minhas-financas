83579d235b7ec7277f0ae52c08f651dff10035d2
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-04-07 22:20:50 -0300

    Adicionando Rotas no Navbar

--- frontend/financas/src/components/navbar.js
+++ frontend/financas/src/components/navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import NavbarItem from './navbaritem';
 
 function Navbar() {
     return(
@@ -19,18 +20,10 @@ function Navbar() {
 
                 <div className="collapse navbar-collapse" id="navbarResponsive">
                     <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <a className="nav-link" href="home.html">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="usuarios.html">Usuários</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="lancamentos.html">Lançamentos</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="login.html">Login</a>
-                        </li>
+                        <NavbarItem href="#/" label="Home"/>
+                        <NavbarItem href="#/cadastro-usuario" label="Usuários"/>
+                        <NavbarItem href="#/" label="Lançamentos"/>
+                        <NavbarItem href="#/login" label="Login"/>
                     </ul>
                 </div>
             </div>
