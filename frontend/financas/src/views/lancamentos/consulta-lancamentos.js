18bcc659633e21c627d893f1ad5105145f02d62c
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-04-09 17:59:41 -0300

    Implementando o Serviço da API de Lançamentos

--- frontend/financas/src/views/lancamentos/consulta-lancamentos.js
+++ frontend/financas/src/views/lancamentos/consulta-lancamentos.js
@@ -9,6 +9,16 @@ import LancamentosTable from './lancamentosTable';
 
 class ConsultaLancamentos extends React.Component {
 
+    state = {
+        ano: '',
+        mes: '',
+        tipo: ''
+    }
+
+    buscar = () => {
+        console.log(this.state);
+    }
+
     render() {
         const meses = [
             {label: 'Selecione...', value: ''},
@@ -46,19 +56,30 @@ class ConsultaLancamentos extends React.Component {
                                     type="text" 
                                     className="form-control" 
                                     id="inputAno" 
-                                    aria-describedby="emailHelp" 
+                                    value={this.state.ano}
+                                    onChange={e => this.setState({ano: e.target.value})}
                                     placeholder="Digite o Ano"/>
                             </FormGroup>
 
                             <FormGroup htmlFor="inputMes" label="Mês: *">
-                                <SelectMenu id="inputMes" className="form-control"  lista={meses}/>
+                                <SelectMenu 
+                                    id="inputMes" 
+                                    value={this.state.mes}
+                                    onChange={e => this.setState({mes: e.target.value})}
+                                    className="form-control"  
+                                    lista={meses}/>
                             </FormGroup>
 
                             <FormGroup htmlFor="inputTipo" label="Tipo Lançamento: *">
-                                <SelectMenu id="inputTipo" className="form-control"  lista={tipos}/>
+                                <SelectMenu 
+                                    id="inputTipo" 
+                                    value={this.state.tipo}
+                                    onChange={e => this.setState({tipo: e.target.value})}
+                                    className="form-control"  
+                                    lista={tipos}/>
                             </FormGroup>
 
-                            <button type="button" className="btn btn-success">Buscar</button>
+                            <button onClick={this.buscar} type="button" className="btn btn-success">Buscar</button>
                             <button type="button" className="btn btn-danger">Cadastrar</button>
                         </div>
                     </div>
