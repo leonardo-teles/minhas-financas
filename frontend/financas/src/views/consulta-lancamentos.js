fc315201cecf77d71854a88d4b45b1d1895a9b01
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-04-08 23:51:22 -0300

    Criando o Componente Select Menu

--- frontend/financas/src/views/consulta-lancamentos.js
+++ frontend/financas/src/views/consulta-lancamentos.js
@@ -3,10 +3,27 @@ import { withRouter } from 'react-router-dom';
 
 import Card from '../components/card';
 import FormGroup from '../components/form-group';
+import SelectMenu from '../components/selectMenu';
 
 class ConsultaLancamentos extends React.Component {
 
     render() {
+        const lista = [
+            {label: 'Selecione...', value: ''},
+            {label: 'Janeiro', value: 1},
+            {label: 'Fevereiro', value: 2},
+            {label: 'Março', value: 3},
+            {label: 'Abril', value: 4},
+            {label: 'Maio', value: 5},
+            {label: 'Junho', value: 6},
+            {label: 'Julho', value: 7},
+            {label: 'Agosto', value: 8},
+            {label: 'Setembro', value: 9},
+            {label: 'Outubro', value: 10},
+            {label: 'Novembro', value: 11},
+            {label: 'Dezembro', value: 12}
+        ];
+
         return(
             <Card title="Consultar Lançamentos">
                 <div className="row">
@@ -15,10 +32,13 @@ class ConsultaLancamentos extends React.Component {
                             <FormGroup htmlFor="inputAno" label="Ano: *">
                                 <input 
                                     type="text" 
-                                    class="form-control" 
+                                    className="form-control" 
                                     id="inputAno" 
                                     aria-describedby="emailHelp" 
                                     placeholder="Digite o Ano"/>
+                            </FormGroup>
+                            <FormGroup htmlFor="inputAno" label="Ano: *">
+                                <SelectMenu className="form-control"  lista={lista}/>
                             </FormGroup>
                         </div>
                     </div>
